8e026eac448760cfc901f0a2ba0c9c7edb885041
ny2060 <dpswpfskdud@naver.com>
2021-01-04 16:07:48 +0900

    [test]:ny

---
+++ app/src/main/java/com/sopt/cherish/util/SimpleLogger.kt
@@ -0,0 +1,10 @@
+package com.sopt.cherish.util
+
+/**
+ * Created on 2020-12-30 by SSong-develop
+ */
+object SimpleLogger {
+    fun logI(message: String) {
+        android.util.Log.i("SSong-develop", message)
+    }
+}
